fc80b929a161a4d13f3e43d62257a14f0db5cd22
HodaTabasi <htabasi95@gmail.com>
2023-10-03 10:13:39 +0300

    تعديلات اخري توثيف الفيديو كوول

--- lib/screens/splach/screen_2_splash.dart
+++ lib/screens/splach/screen_2_splash.dart
@@ -6,9 +6,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../helper/SizeConfig.dart';
 
-import '../../helper/SizeConfig.dart';
-import '../../preferences/shared_pref_controller.dart';
-
 class Screen2Splash extends StatefulWidget {
   static String routeName = "/splash";
   @override
@@ -41,78 +38,76 @@ class _Screen2SplashState extends State<Screen2Splash> with TickerProviderStateM
       });
     },);
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/images/background.png'),
-                  fit: BoxFit.fitWidth)),
-          child: Center(
-              child: Stack(
-                children: [
-                  // const Spacer(),
-                  AnimatedPositioned(
-                    height: play?MediaQuery.of(context).size.height/0.9 : 0,
-                    left: 0,
-                    right: 0,
-                    bottom: play?100:0,
-                    duration: Duration(milliseconds: 1200),
-                      curve: Curves.easeInOut,
-                      child: Image.asset('assets/images/logo.png',)),
-                  // SizedBox(height: 40.h,),
-                  AnimatedPositioned(
-                    height: play?MediaQuery.of(context).size.height/2.6 : 0,
-                    left: 0,
-                    right: 0,
-                    bottom: play?100:0,
-                    duration: Duration(milliseconds: 1300),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.fitWidth)),
+        child: Center(
+            child: Stack(
+              children: [
+                // const Spacer(),
+                AnimatedPositioned(
+                  height: play?MediaQuery.of(context).size.height/0.9 : 0,
+                  left: 0,
+                  right: 0,
+                  bottom: play?100:0,
+                  duration: Duration(milliseconds: 1200),
                     curve: Curves.easeInOut,
-                    child: Text(
-                      '''مـــرحـــبــًا بــكــم فــي مـــســـتـــشـــفـــى
+                    child: Image.asset('assets/images/logo.png',)),
+                // SizedBox(height: 40.h,),
+                AnimatedPositioned(
+                  height: play?MediaQuery.of(context).size.height/2.6 : 0,
+                  left: 0,
+                  right: 0,
+                  bottom: play?100:0,
+                  duration: Duration(milliseconds: 1300),
+                  curve: Curves.easeInOut,
+                  child: Text(
+                    '''مـــرحـــبــًا بــكــم فــي مـــســـتـــشـــفـــى
 أبــــهـــا الـــخـــاص الـــطـــبـــي''',
-                      style: TextStyle(color: Color(0xff0E4C8F), fontSize: 22.sp
-                          ,fontFamily: 'Tajawal',fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
+                    style: TextStyle(color: Color(0xff0E4C8F), fontSize: 22.sp
+                        ,fontFamily: 'Tajawal',fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
                   ),
-                  // SizedBox(height: 60.h,),
-                  AnimatedPositioned(
-                    height: play?MediaQuery.of(context).size.height/3.4 : 0,
-                    left: 0,
-                    right: 0,
-                    bottom: play?100:0,
-                    duration: Duration(milliseconds: 1400),
-                    curve: Curves.easeInOut,
-                    child: Text('التطور والريادة',style: TextStyle(color: Color(0xff058638), fontSize: 16.sp,fontFamily: 'atef',fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,),
-                  ),
-                  // SizedBox(height: 60.h,),
-                  AnimatedPositioned(
-                    height: play?MediaQuery.of(context).size.height/2.7 : 0,
-                    left: 0,
-                    right: 0,
-                    bottom: play?50:0,
-                    duration: Duration(milliseconds: 1500),
+                ),
+                // SizedBox(height: 60.h,),
+                AnimatedPositioned(
+                  height: play?MediaQuery.of(context).size.height/3.4 : 0,
+                  left: 0,
+                  right: 0,
+                  bottom: play?100:0,
+                  duration: Duration(milliseconds: 1400),
+                  curve: Curves.easeInOut,
+                  child: Text('التطور والريادة',style: TextStyle(color: Color(0xff058638), fontSize: 16.sp,fontFamily: 'atef',fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,),
+                ),
+                // SizedBox(height: 60.h,),
+                AnimatedPositioned(
+                  height: play?MediaQuery.of(context).size.height/2.7 : 0,
+                  left: 0,
+                  right: 0,
+                  bottom: play?50:0,
+                  duration: Duration(milliseconds: 1500),
+                  curve: Curves.easeInCubic,
+                  child: AvatarGlow(
+                    endRadius: 35.0.r,
+                    showTwoGlows: true,
+                    glowColor: Color(0xff8CABCD),
+                    animate: true,
                     curve: Curves.easeInCubic,
-                    child: AvatarGlow(
-                      endRadius: 35.0.r,
-                      showTwoGlows: true,
-                      glowColor: Color(0xff8CABCD),
-                      animate: true,
-                      curve: Curves.easeInCubic,
-                      child: InkWell(
-                        onTap: () => Navigator.pushReplacementNamed(context, SelectLanguage.routeName),
-                        child: Image.asset(
-                          'assets/images/icon1.png',
-                          height: 50.h,
-                        ),
+                    child: InkWell(
+                      onTap: () => Navigator.pushReplacementNamed(context, SelectLanguage.routeName),
+                      child: Image.asset(
+                        'assets/images/icon1.png',
+                        height: 50.h,
                       ),
                     ),
                   ),
+                ),
 //                 const Spacer()
-                ],
-              )),
-        ),
+              ],
+            )),
       ),
     );
   }
